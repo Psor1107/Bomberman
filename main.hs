be19c8e4863d9a231194ed2397514c21a8fd14f1
Gabriel Sanchez <psor1107@gmail.com>
2021-11-05 20:55:10 -0300

    Movimentação 2

--- main.hs
+++ main.hs
@@ -1,3 +1,4 @@
+import Data.Graph (Table)
 {-
 
 Gabriel Luchetti Garcia Sanchez - 12011BCC015
@@ -5,25 +6,65 @@ Silvano Martins da Silva Junior - 12011BCC042
 
 -}
 
-data ItemJogador = Patins | Bomba | Arremesso
-data Item = Grama | ItemJogador | Parede | Pedra | Jogador Int deriving Eq
+data ItemJogador = Patins | Bombas | Arremesso deriving (Eq, Show)
+data Item = Grama | Presente ItemJogador | Parede | Bomba | Pedra | Jogador Int deriving (Eq, Show)
 
 type Celula = [Item]
-type Linha = (Celula, Celula, Celula, Celula, Celula, Celula, Celula, Celula)
-type Tabuleiro = (Linha, Linha, Linha, Linha, Linha, Linha, Linha, Linha)
+type Linha = [Celula]
+type Tabuleiro = [Linha]
 type CapacidadeJogador = ((ItemJogador, Int) , (ItemJogador, Int), (ItemJogador, Int))
 type Cordenada = (Int, Int)
-data Direcao = N | S | L | O
+data Direcao = N | S | L | O deriving (Eq,Show)
 type Jogador = (Int, Cordenada, Direcao, CapacidadeJogador)
 
+cel1 :: Celula
+cel1 = [Pedra]
+cel2 :: Celula
+cel2 = [Grama]
+cel3 :: Celula
+cel3 = [Parede, Grama]
+cel4 :: Celula
+cel4 = [Presente Patins, Grama]
+cel5 :: Celula
+cel5 = [Presente Bombas, Grama]
+cel6 :: Celula
+cel6 = [Presente Arremesso, Grama]
+cel7 :: Celula
+cel7 = [Jogador 1, Grama]
+cel8 :: Celula
+cel8 = []
+
+lin1 :: Linha
+lin1 = [cel1, cel1, cel1, cel1, cel1, cel1, cel1, cel1]
+lin2 :: Linha
+lin2 = [cel1, cel7, cel2, cel2, cel2, cel2, cel2, cel1]
+lin3 :: Linha
+lin3 = [cel1, cel2, cel3, cel2, cel2, cel3, cel2, cel1]
+lin4 :: Linha
+lin4 = [cel1, cel2, cel2, cel2, cel2, cel2, cel2, cel1]
+lin5 :: Linha
+lin5 = [cel1, cel2, cel2, cel2, cel2, cel2, cel2, cel1]
+lin6 :: Linha
+lin6 = [cel1, cel2, cel3, cel2, cel2, cel3, cel2, cel1]
+lin7 :: Linha
+lin7 = [cel1, cel2, cel2, cel2, cel2, cel2, cel2, cel1]
+lin8 :: Linha
+lin8 = [cel1, cel1, cel1, cel1, cel1, cel1, cel1, cel1]
+
+tabuleiroPadrao :: Tabuleiro
+tabuleiroPadrao = [lin1, lin2, lin3, lin4, lin5, lin6, lin7, lin8]
 
 regrasCelula :: Celula -> Bool
 regrasCelula celula
   | length celula > 1 && last celula /= Grama = False
-  | length celula == 1 && (head celula /= Grama || head celula /= Pedra) = False
-  | length celula == 2 && (head celula == ItemJogador && last celula /= Grama) = False
+  | length celula == 1 && (head celula /= Grama && head celula /= Pedra) = False
+  | length celula == 2 && (head celula == Presente Patins && last celula /= Grama) = False
+  | length celula == 2 && (head celula == Presente Arremesso && last celula /= Grama) = False
+  | length celula == 2 && (head celula == Presente Bombas && last celula /= Grama) = False
   | length celula == 2 && (head celula == Parede && last celula /= Grama) = False
-  | length celula == 3 && (head celula == Parede && head (tail celula) /= ItemJogador)  = False
+  | length celula == 3 && (head celula == Parede && head (tail celula) /= Presente Patins)  = False
+  | length celula == 3 && (head celula == Parede && head (tail celula) /= Presente Bombas)  = False
+  | length celula == 3 && (head celula == Parede && head (tail celula) /= Presente Arremesso)  = False
   | length celula == 2 && (head celula == Jogador 1 && last celula /= Grama) = False
   | length celula == 2 && (head celula == Jogador 2 && last celula /= Grama) = False
   | length celula == 2 && (head celula == Jogador 3 && last celula /= Grama) = False
@@ -36,15 +77,69 @@ regrasCelula celula
   | otherwise = True
 
 linhaValida :: Linha -> Bool
-linhaValida (c1,c2,c3,c4,c5,c6,c7,c8)
+linhaValida [c1,c2,c3,c4,c5,c6,c7,c8]
   | regrasCelula c1 && regrasCelula c2 && regrasCelula c3 && regrasCelula c4 && regrasCelula c5 && regrasCelula c6 && regrasCelula c7 && regrasCelula c8 = True
   | otherwise = False
 
-
-constroi_tabuleiro :: Tabuleiro -> Tabuleiro
-constroi_tabuleiro (l1,l2,l3,l4,l5,l6,l7,l8)
- | linhaValida l1 && linhaValida l2 && linhaValida l3 && linhaValida l4 && linhaValida l5 && linhaValida l6 && linhaValida l7 && linhaValida l8 = (l1,l2,l3,l4,l5,l6,l7,l8)
+constroiTabuleiro :: Tabuleiro -> Tabuleiro
+constroiTabuleiro [l1,l2,l3,l4,l5,l6,l7,l8]
+ | linhaValida l1 && linhaValida l2 && linhaValida l3 && linhaValida l4 && linhaValida l5 && linhaValida l6 && linhaValida l7 && linhaValida l8 = [l1,l2,l3,l4,l5,l6,l7,l8]
  | otherwise = error "Tabuleiro Inválido!"
+
+jogador :: Item -> Jogador
+jogador jogador
+  | jogador == Jogador 1 = (1, (2,2), S, ((Patins,1), (Bombas, 1), (Arremesso,0))) 
+  | otherwise = error "Jogador não encontrado"
+
+checaLinha :: Linha -> Item -> Celula
+checaLinha linha jogador
+  | null linha = []
+  | head (head linha) == jogador = head linha
+  | otherwise = checaLinha(tail linha) jogador
+
+achaJogador :: Tabuleiro -> Item -> Celula
+achaJogador tabuleiro jogador
+  | checaLinha (head tabuleiro) jogador /= [] = checaLinha (head tabuleiro) jogador
+  | otherwise = achaJogador (tail tabuleiro) jogador
+
+movimentarS :: Jogador -> Jogador
+movimentarS (a, (b1, b2), c, d)
+  | b2 < 7 && head (pegaLinha tabuleiroPadrao (b2 + 1) b1) /= Parede && head (pegaLinha tabuleiroPadrao (b2 + 1) b1) /= Bomba = (a, (b1, b2 + 1), c, d)
+  | otherwise = error "Movimento Inválido"
+
+movimentarN :: Jogador -> Jogador
+movimentarN (a, (b1, b2), c, d)
+  | b2 > 2 && head (pegaLinha tabuleiroPadrao (b2 - 1) b1) /= Parede && head (pegaLinha tabuleiroPadrao (b2 - 1) b1) /= Bomba = (a, (b1, b2 - 1), c, d)
+  | otherwise = error "Movimento Inválido"
+
+movimentarO :: Jogador -> Jogador
+movimentarO (a, (b1, b2), c, d)
+  | b1 > 2 && head (pegaLinha tabuleiroPadrao b2 (b1 - 1)) /= Parede && head (pegaLinha tabuleiroPadrao b2 (b1 - 1)) /= Bomba = (a, (b1 - 1, b2), c, d)
+  | otherwise = error "Movimento Inválido"
+
+movimentarL :: Jogador -> Jogador
+movimentarL (a, (b1, b2), c, d)
+  | b2 < 7 && head (pegaLinha tabuleiroPadrao b2 (b1 + 1)) /= Parede && head (pegaLinha tabuleiroPadrao b2 (b1 + 1)) /= Bomba = (a, (b1 + 1, b2), c, d)
+  | otherwise = error "Movimento Inválido"
+
+movimentar :: Jogador -> Direcao -> Jogador
+movimentar jogador direcao
+  | direcao == S = movimentarS jogador
+  | direcao == N = movimentarN jogador
+  | direcao == O = movimentarO jogador
+  | direcao == L = movimentarL jogador
+
+pegaCelula :: Linha -> Int -> Celula
+pegaCelula linha cel
+  | cel == 1 = head linha
+  | otherwise = pegaCelula (tail linha) (cel-1)
+
+pegaLinha :: Tabuleiro -> Int -> Int -> Celula
+pegaLinha tabuleiro linha cel
+  | linha == 1 = pegaCelula (head tabuleiro) cel
+  | otherwise = pegaLinha (tail tabuleiro) (linha-1) cel
+
+{-
 
 Arremesso :: Tabuleiro -> Jogador -> Tabuleiro
 Arremesso (l1,l2,l3,l4,l5,l6,l7,l8) (a, b, c, d)
@@ -52,3 +147,5 @@ Arremesso (l1,l2,l3,l4,l5,l6,l7,l8) (a, b, c, d)
   | c == 'N' && Bomba `elem` ((l1,l2,l3,l4,l5,l6,l7,l8) !! fst b-1) !! snd b =
   | c == 'L' && Bomba `elem` ((l1,l2,l3,l4,l5,l6,l7,l8) !! fst b) !! snd b+1 =
   | c == 'O' && Bomba `elem` ((l1,l2,l3,l4,l5,l6,l7,l8) !! fst b) !! snd b-1 =
+
+-}
